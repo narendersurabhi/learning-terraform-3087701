4a7747555ce48af2892a9452b7d014cfe393e350
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-08 23:46:04 -0400

    vpc security group rule config

--- main.tf
+++ main.tf
@@ -21,4 +21,48 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "HelloWorld"
   }
+
+  vpc_security_group_ids = [aws_security_group.blog.id]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "blog" {
+  vpc_id = data.aws_vpc.default.id
+  name   = "blog"
+  tags   = {
+    Terraform = "true"
+  }
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  security_group_id = aws_security_group.blog.id
+
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  security_group_id = aws_security_group.blog.id
+
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "blog_all_out" {
+  security_group_id = aws_security_group.blog.id
+
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 }
