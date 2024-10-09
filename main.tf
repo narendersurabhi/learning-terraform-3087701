348339109f58102c9e43877cd952e0fd65ac2032
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-09 07:58:49 -0400

    Update main.tf
    New Module security group

--- main.tf
+++ main.tf
@@ -22,11 +22,24 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.security-group-module.security_group_id]
 }
 
 data "aws_vpc" "default" {
   default = true
+}
+
+module "security-group-module" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  
+  ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group" "blog" {
