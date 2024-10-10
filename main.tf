930652ff3633089d58e6ce54e15514417ac3ea6a
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-10 00:55:58 -0400

    updated target_groups

--- main.tf
+++ main.tf
@@ -50,19 +50,14 @@ module "blog_alb" {
   }
 
   target_groups = {
-    instance = {
-      name_prefix = "blog-"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
     }
-  }
+  }  
 
   tags = {
     Environment = "dev"    
