1fed4b8cfa5034c71bb14e8115f1a92405025af2
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-10 11:49:20 -0400

    updated alb to target asg

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      target_id        = module.blog_asg.id
     }
   }  
 
