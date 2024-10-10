5bb2ddb1231bbdc403de1f883bb2dc6a81e996cb
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-09 23:33:17 -0400

    changed security group

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "aws_vpc" "default" {
 module "security_group_module" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name = "blog_module_sg"
+  name = "blog_sg"
   
   ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -41,42 +41,4 @@ module "security_group_module" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 
   vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group" "blog" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "blog"
-  tags   = {
-    Terraform = "true"
-  }
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  security_group_id = aws_security_group.blog.id
-
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  security_group_id = aws_security_group.blog.id
-
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_all_out" {
-  security_group_id = aws_security_group.blog.id
-
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
 }
