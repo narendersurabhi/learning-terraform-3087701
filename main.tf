d4e21d515c1044e78c37255354c417a26a8b0ef7
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-12 11:04:47 -0400

    changed from modules to resources

--- main.tf
+++ main.tf
@@ -15,27 +15,206 @@ data "aws_ami" "app_ami" {
 }
 
 
-module "blog_asg" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.0"
-  # insert the 1 required variable 
-  
-  name = "blog"
-  min_size = 1
-  max_size = 10
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  vpc_zone_identifier  = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type  
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Environment = "dev"    
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+/*
+module "blog_lb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  name = "blog_lb_sg"
+  
+  ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  vpc_id = module.blog_vpc.vpc_id
+}
+
+module "blog_as_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  name = "blog_as_sg"
+  
+  ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
+  ingress_cidr_blocks = [module.blog_lb_sg.security_group_id]
+  
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  vpc_id = module.blog_vpc.vpc_id
+}
+*/
+
+resource "aws_launch_configuration" "blog_template" {
+  name_prefix     = "aws-asg-"
+  image_id        = data.aws_ami.app_ami.id
+  instance_type   = var.instance_type  
+  user_data       = file("user-data.sh")
+  security_groups = [aws_security_group.sg_web.id] 
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+/*
+resource "aws_launch_template" "foobar" {
+  name_prefix   = "foobar"
+  image_id      = "ami-1a2b3c"
+  instance_type = "t2.micro"
+}
+*/
+
+resource "aws_autoscaling_group" "blog_asg" {
+  availability_zones = ["us-east-2a", "us-east-2b"]
+  desired_capacity   = 1
+  max_size           = 3
+  min_size           = 1
+  name = "blog"
+
+  vpc_zone_identifier   = module.blog_vpc.public_subnets
+  security_groups = [aws_security_group.sg_web.id] 
+
+  launch_configuration = aws_launch_configuration.blog_template.name
+  
+  /*
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = aws_launch_template.example.latest_version
+  }
+  */
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }  
+
+  tag {
+    key                 = "Key"
+    value               = "Value"
+    propagate_at_launch = true
   }
 
 }
 
+
+resource "aws_lb_target_group" "blog_tg" {
+  name     = "blog-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+}
+
+resource "aws_lb" "blog_alb" {
+  name               = "blog_alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_lb.id]
+  subnets            = module.blog_vpc.public_subnets
+}
+
+resource "aws_lb_listener" "blog_lb_lstnr" {
+  load_balancer_arn = aws_lb.blog_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.blog_tg.arn
+  }
+}
+
+
+resource "aws_lb_target_group" "blog_tg" {
+  name     = "blog-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+}
+
+resource "aws_autoscaling_attachment" "auto_to_targ" {
+  autoscaling_group_name = aws_autoscaling_group.blog_asg.id
+  alb_target_group_arn   = aws_lb_target_group.blog_tg.arn
+}
+
+resource "aws_security_group" "sg_lb" {
+  name = "sg-lb"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = module.blog_vpc.vpc_id
+}
+
+resource "aws_security_group" "sg_web" {
+  name = "web-sg"
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_lb.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  vpc_id = module.blog_vpc.vpc_id
+}
+
+/*
+resource "aws_lb_target_group" "blog_tg" {
+  name     = "blog-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+
+  target_group_health {
+    dns_failover {
+      minimum_healthy_targets_count      = "1"
+      minimum_healthy_targets_percentage = "off"
+    }
+
+    unhealthy_state_routing {
+      minimum_healthy_targets_count      = "1"
+      minimum_healthy_targets_percentage = "off"
+    }
+  }
+}
+*/
+
+/*
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
@@ -44,7 +223,7 @@ module "blog_alb" {
   subnets = module.blog_vpc.public_subnets
 
   # Security Group
-  security_groups = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_lb_sg.security_group_id]
 
   listeners = {
     ex-http-https-redirect = {
@@ -72,32 +251,5 @@ module "blog_alb" {
     Environment = "dev"    
   }
 }
+*/
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["usw2-az1", "usw2-az2", "usw2-az3", "usw2-az4"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
-  name = "blog_new"
-  
-  ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
-  vpc_id = module.blog_vpc.vpc_id
-}
